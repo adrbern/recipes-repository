5f3e4859a32fb4d564d122da0cba160b88618c70
Adrian Bernabe <adrian.bernabe@bbvaitspain.com>
2021-11-05 21:44:11 +0100

    init server config and database

---
+++ models/server.js
@@ -0,0 +1,47 @@
+const express = require('express');
+const cors = require('cors');
+const { dbConnection } = require('../database/config');
+
+class Server {
+    constructor() {
+        this.app = express();
+        this.port = process.env.PORT;
+        this.usuariosPath = '/api/xxx';
+
+        //Conexion BD
+        this.conectarDB();
+
+        //Middlewares
+        this.middlewares();
+
+        //Rutas de mi aplicacion
+        this.routes();
+    }
+
+    async conectarDB() {
+        await dbConnection();
+    }
+
+    routes() {
+        this.app.use(this.authPath, require('../routes/xxx'));
+    }
+
+    middlewares() {
+        // CORS
+        this.app.use(cors());
+
+        // Lectura y parseo del body
+        this.app.use(express.json());
+
+        // Directorio estatico
+        this.app.use(express.static('public'));
+    }
+
+    listen() {
+        this.app.listen(this.port, () => {
+            console.log('Servidor corriendo en el puerto' + this.port);
+        });
+    }
+}
+
+module.exports = Server;
