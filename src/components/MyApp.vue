cc8c9999e8d97cd16680c39c04f6ae4b9a381451
Adrian Bernabe <adrbern@gmail.com>
2021-12-07 14:40:13 +0100

    basic search-recipes and sign-in componentes

--- src/components/MyApp.vue
+++ src/components/MyApp.vue
@@ -3,30 +3,10 @@
         <HeaderPage>header loading...</HeaderPage>
 
         <div class="banner wow fadeInUp" data-wow-delay="0.4s">
-            <div class="container_wrap">
-                <h1>¿De qué dispones?</h1>
-                <div class="dropdown-buttons">
-                    <div class="dropdown-button">
-                        <select class="dropdown" tabindex="9" data-settings='{"wrapperClass":"flat"}'>
-                            <option value="0"> 15 minutos</option>
-                            <option value="1"> 30 minutos</option>
-                            <option value="2"> 40 minutos</option>
-                            <option value="3"> 1 hora</option>
-                            <option value="4"> 2 horas</option>
-                            <option value="5"> 3 horas </option>
-                            <option value="6"> Más de 3 horas</option>
-                        </select>
-                    </div>
-                </div>
-                <form>
-                    <input type="text" value="Aceite, sal, ternera, ...'" onfocus="this.value = '';" onblur="if (this.value == '') {this.value = 'Aceite, sal, ternera, ...';}">
-                    <div class="contact_btn">
-                        <a href="./living.html"><label class="btn1 btn-2 btn-2g">Buscar</label></a>
-                    </div>
-                </form>
-                <div class="clearfix"></div>
-            </div>
+           <SearchRecipes>search loading...</SearchRecipes>
         </div>
+
+        <!--Submenu>submenu loading...</Submenu-->
 
             <div id="CUERPO">Loading...</div>
 
@@ -82,29 +62,20 @@
 
 <script>
 import HeaderPage from './HeaderPage.vue';
+import SearchRecipes from './SearchRecipes.vue'
+import Submenu from './Submenu.vue'
+
 
 export default {
   name: 'MyApp',
   components: {
-    HeaderPage
+    HeaderPage,
+    SearchRecipes,
+    Submenu
   }
 }
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-h3 {
-  margin: 40px 0 0;
-}
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
-}
 </style>
---
+++ src/components/SignIn.vue
@@ -0,0 +1,38 @@
+<template>
+    <div id="loginContainer">
+        <a href="#" id="loginButton"><img src="images/login.png"><span>Iniciar Sesión</span></a>
+        <div id="loginBox">
+            <div id="loginForm">
+                <fieldset id="body">
+                    <fieldset>
+                            <label for="email">Usuario</label>
+                            <input type="text" name="email" id="email">
+                    </fieldset>
+                    <fieldset>
+                            <label for="password">Contraseña</label>
+                            <input type="password" name="password" id="password">
+                        </fieldset>
+                    <a href="./entertain.html"><input type="submit" id="login" value="Iniciar "></a>
+                    <label for="checkbox"><input type="checkbox" id="checkbox"> <i>Recordar</i></label>
+                </fieldset>
+                    <span><a href="#">Olvidaste tu contraseña?</a></span>
+            </div>
+        </div>
+
+        <!--div id="logged"><a href="./entertain.html"><img src="images/logged.png"><span>Federico</span></a></div-->
+
+        <div class="clearfix"></div>
+    </div>
+</template>
+
+<script>
+export default {
+  name: 'SignIn',
+  components: {
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+</style>
