dd4ca6026682d8b2462eef0e105a464ae6f73f4c
Adrian Bernabe <adrbern@gmail.com>
2021-12-13 20:07:33 +0100

    routers

--- src/router/router.js
+++ src/router/router.js
@@ -1,10 +1,20 @@
 import { createRouter, createWebHashHistory } from 'vue-router';
 
 import Home from '../views/MyApp';
+import AdvancedSearch from '../views/AdvancedSearch';
+import Profile from '../views/Profile';
+import Recipe from '../views/Recipe';
+import NewRecipe from '../views/NewRecipe';
+import SignIn from '../views/SignIn';
 import About from '../views/About';
 
 const routes = [
     { path: '/', component: Home },
+    { path: '/advanced-search', component: AdvancedSearch },
+    { path: '/profile', component: Profile },
+    { path: '/recipe', component: Recipe },
+    { path: '/new-recipe', component: NewRecipe },
+    { path: '/sign-in', component: SignIn },
     { path: '/about', component: About }
 ];
 
