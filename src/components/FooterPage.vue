e12b9d59bceb547df043e53170f538255e2be296
Adrian Bernabe <adrbern@gmail.com>
2021-12-26 21:20:22 +0100

    footer basic

--- src/components/FooterPage.vue
+++ src/components/FooterPage.vue
@@ -2,27 +2,18 @@
     <div class="footer">
         <div class="container">
             <div class="footer_grids">
-                <div class="footer-grid">
+                <div class="col-md-4 footer-grid">
                     <h4>Recipes On Time</h4>
                     <ul class="list1">
                         <li><a href="contact.html">Contactar</a></li>
                         <li><a href="#">Política de Privacidad</a></li>
                         <li><a href="#">Condiciones de uso</a></li>
-                        <li><a href="#">Sugerencias</a></li>
                     </ul>
                 </div>
-                <div class="footer-grid">
-                    <h4>Enlaces de interés</h4>
-                    <ul class="list1">
-                        <li><a href="#">Cocina Europea</a></li>
-                        <li><a href="#">Gastronomía americana</a></li>
-                        <li><a href="#">Estrellas Michelín </a></li>
-                        <li><a href="#">Cocina asiática</a></li>
-                    </ul>
-                </div>
-                <div class="footer-grid last_grid">
+                <div class="col-md-4"></div>
+                <div class="col-md-4 footer-grid last_grid">
                     <h4>Redes Sociales</h4>
-                    <ul class="footer_social wow fadeInLeft" data-wow-delay="0.4s">
+                    <ul class="footer_social wow fadeInRight" data-wow-delay="0.4s">
                         <li><a href=""> <i class="fb"> </i> </a></li>
                         <li><a href=""><i class="tw"> </i> </a></li>
                         <li><a href=""><i class="google"> </i> </a></li>
