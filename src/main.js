dce027b90a149f293514f6f7908f0748d7114ede
Adrian Bernabe <adrbern@gmail.com>
2022-01-02 14:55:37 +0100

    multilanguage

--- src/main.js
+++ src/main.js
@@ -1,7 +1,14 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router/router'
+import VueI18n from 'vue-i18n';
+
+const i18n = new VueI18n({
+    locale: 'en',
+    messages
+});
 
 createApp(App)
     .use(router)
+    .use(i18n)
     .mount('#app');
